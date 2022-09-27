b5b6ee93a3310f4431ef46a347d8b1c2034fdf0c
jaswanth <jaswanthbeere@gmail.com>
2022-09-28 01:02:21 +0530

    added navbar

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
-import First from '../components/first.js'
-import Second from '../components/second'
-import Third from '../components/third'
-import Fourth from '../components/fourth'
-import Fifth from '../components/fifth'
+import First from "../components/first.js";
+import Second from "../components/second";
+import Third from "../components/third";
+import Fourth from "../components/fourth";
+import Fifth from "../components/fifth";
+import Navbar from "../components/Navbar";
 export default function Home() {
   return (
     <>
-        <First/>
-        <Second/>
-        <Third/>
-        <br /><br />
-        <Fourth/>
-        <br />
-        <Fifth/>
+      <Navbar />
+      <First />
+      <Second />
+      <Third />
+      <br />
+      <br />
+      <Fourth />
+      <br />
+      <Fifth />
     </>
-  )
+  );
 }
