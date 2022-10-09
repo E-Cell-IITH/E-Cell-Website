52e20c919b1c334312e1ebdc5fbca3cdd8910352
jaswanth <jaswanthbeere@gmail.com>
2022-10-09 23:37:56 +0530

    added preLoader

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,43 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import LoadingScreen from "../components/LoadingScreen";
+import React from "react";
+import { BounceLoader } from "react-spinners";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = React.useState(false);
+  // React.useEffect(() => {
+  //   setLoading(true);
+  // }, []);
+  React.useEffect(() => {
+    setLoading(true);
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+  const override = {
+    display: "block",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    borderColor: "red",
+  };
+  return (
+    <>
+      {loading ? (
+        <BounceLoader
+          color={"#7a4daf"}
+          loading={loading}
+          cssOverride={override}
+          size={150}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      ) : (
+        <React.Fragment>
+          <Component {...pageProps} />
+        </React.Fragment>
+      )}
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,11 @@ import Third from "../components/third";
 import Fourth from "../components/fourth";
 import Fifth from "../components/fifth";
 import Navbar from "../components/Navbar";
+import { useState, useEffect } from "react";
+
 export default function Home() {
+  // const [loading, setLoading] = useState(false);
+  // useEffect
   return (
     <>
       <Navbar />
