33f0fd7e3b6a70157451401f00a37c5edd6daf10
Maharshi <cs21btech11027@iith.ac.in>
2022-10-30 12:12:26 +0530

    added caportal to 'ecell.iith.ac.in/caportal' route

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites(){
+   return [
+    {
+      source:'/caportal',
+      destination:'/caprg/index.html'
+    }
+    ]
+  },
   images: {
     domains: ["res.cloudinary.com"],
   },
