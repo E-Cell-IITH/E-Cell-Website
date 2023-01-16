6a0b3c955c601a0dee6c3dc6cefe57ffd40bd7cf
Maharshi <cs21btech11027@iith.ac.in>
2023-01-16 17:16:42 +0530

    error solved

--- pages/esummit.js
+++ pages/esummit.js
@@ -1,25 +1,4 @@
-<<<<<<< HEAD
-import First from '../components/esummit';
-import Events from '../components/Events/Events';
-import Keynote from '../components/Events/Keynote';
-import KeySpeakers from '../components/Events/KeySpeakers';
-import Speakers from '../components/Events/Speakers';
-import Workshops from '../components/workshops';
 
-export default function Home() {
-  return (
-    <div className="bg-dark">
-      <First />
-      {/* <Keynote /> */}
-
-      <KeySpeakers />
-      <Workshops />
-      <Events />
-      {/* <Speakers /> */}
-    </div>
-  );
-}
-=======
 import First from '../components/esummit';
 import Events from '../components/Events/Events';
 import Keynote from '../components/Events/Keynote';
@@ -42,4 +21,3 @@ export default function Home() {
     </div>
   );
 }
->>>>>>> 691097c33331f390add2fc9b05e8869673a6e6ed
