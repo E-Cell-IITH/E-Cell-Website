e6d7a5d96a63db00efa9e8a511c076a4ddf3c194
ecell-iith <ecell@iith.ac.in>
2023-01-18 00:33:39 +0530

    added panel discussions

--- components/Panel.js
+++ components/Panel.js
@@ -1,47 +1,61 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
-import MuiAccordion from '@mui/material/Accordion';
-import MuiAccordionSummary from '@mui/material/AccordionSummary';
-import MuiAccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import MuiAccordion from "@mui/material/Accordion";
+import MuiAccordionSummary from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import Image from "next/image";
 
 const Accordion = styled((props) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
 ))(({ theme }) => ({
-  border: `1px solid ${theme.palette.divider}`,
-  '&:not(:last-child)': {
+  border: `0px solid rgb(11 12 16)`,
+  "&:not(:last-child)": {
     borderBottom: 0,
   },
-  '&:before': {
-    display: 'none',
+  "&:before": {
+    display: "none",
   },
 }));
 const AccordionSummary = styled((props) => (
   <MuiAccordionSummary
-    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    expandIcon={
+      <ArrowForwardIosSharpIcon
+        sx={{ fontSize: "0.9rem", color: "rgb(69 162 158)" }}
+      />
+    }
     {...props}
   />
 ))(({ theme }) => ({
-  backgroundColor:
-    theme.palette.mode === 'dark'
-      ? 'rgba(255, 255, 255, .05)'
-      : 'rgba(0, 0, 0, .03)',
-  flexDirection: 'row-reverse',
-  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
-    transform: 'rotate(90deg)',
+  backgroundColor: "rgb(11 12 16)",
+  flexDirection: "row-reverse",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
   },
-  '& .MuiAccordionSummary-content': {
+  "& .MuiAccordionSummary-content": {
     marginLeft: theme.spacing(1),
   },
 }));
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  backgroundColor: "rgb(11 12 16)",
   padding: theme.spacing(2),
-  borderTop: '1px solid rgba(0, 0, 0, .125)',
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
 }));
 
+const title_data = [
+  "Transformation of India's Digital Payment Landscape",
+  "Technological Innovations - The driving factors and Impact",
+  "The Employer-Employee Paradox: Concurrency of Layoffs and Hiring",
+];
+const img_data = {
+  "Transformation of India's Digital Payment Landscape": [],
+  "Technological Innovations - The driving factors and Impact": [],
+  "The Employer-Employee Paradox: Concurrency of Layoffs and Hiring": [],
+};
+
 const Panel = () => {
-  const [expanded, setExpanded] = React.useState('panel1');
+  const [expanded, setExpanded] = React.useState("panel1");
 
   const handleChange = (panel) => (event, newExpanded) => {
     setExpanded(newExpanded ? panel : false);
@@ -51,65 +65,239 @@ const Panel = () => {
       <div className="text-5xl mb-20 text-blue text-center font-medium">
         PANEL DISCUSSIONS
       </div>
-      {/* <ul className="sm:text-xl text-md list-disc">
-        <li>Transformation of India&apos;s Digital Payment Landscape </li>
-        <li className="py-5">
-          Technological Innovations - The driving factors and Impact
-        </li>
-        <li>
-          The Employer-Employee Paradox: Concurrency of Layoffs and Hiring
-        </li>
-      </ul> */}
-      <div>
+      <div className="mx-auto flex flex-col justify-center items-center">
         <Accordion
-          expanded={expanded === 'panel1'}
-          onChange={handleChange('panel1')}
+          expanded={expanded === "panel1"}
+          onChange={handleChange("panel1")}
         >
           <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-            <Typography>Collapsible Group Item #1</Typography>
+            <Typography
+              sx={{
+                color: "white",
+                fontSize: { xs: "1rem", sm: "1.35rem" },
+              }}
+            >
+              Transformation of India&apos;s Digital Payment Landscape
+            </Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
+            <div>
+              <div className="flex flex-col sm:flex-row  mx-auto justify-center items-center gap-5 md:gap-10 lg:gap-[3.5rem]">
+                <div className="flex flex-col-reverse justify-center items-center gap-3">
+                  <p className="text-white uppercase text-center text-[1rem]">
+                    Hussaini S F
+                  </p>
+                  <Image
+                    height={200}
+                    width={200}
+                    alt="testing"
+                    src="/hussainisf.jpeg"
+                  />
+                </div>
+                <div className="flex flex-col-reverse justify-center items-center gap-3">
+                  <p className="text-white uppercase text-center text-[1rem]">
+                    N Srinivasan
+                  </p>
+                  <Image
+                    height={200}
+                    width={200}
+                    alt="testing"
+                    src="/srinivasan.jpg"
+                  />
+                </div>
+                {/* <div className="flex flex-col-reverse justify-center items-center gap-3">
+                  <p className="text-white uppercase text-center text-[1rem]">
+                    N Srinivasan
+                  </p>
+                  <Image
+                    height={200}
+                    width={200}
+                    alt="testing"
+                    src="/srinivasan.jpg"
+                  />
+                </div> */}
+              </div>
+              {/* <div className="flex flex-col sm:flex-row mx-auto justify-center items-center gap-5 md:gap-10 lg:gap-[3.5rem mt-10">
+                <div className="flex flex-col-reverse justify-center items-center gap-3">
+                  <p className="text-white uppercase text-center text-[1rem]">
+                    N Srinivasan
+                  </p>
+                  <Image
+                    height={200}
+                    width={200}
+                    alt="testing"
+                    src="/srinivasan.jpg"
+                  />
+                </div>
+                <div className="flex flex-col-reverse justify-center items-center gap-3">
+                  <p className="text-white uppercase text-center text-[1rem]">
+                    N Srinivasan
+                  </p>
+                  <Image
+                    height={200}
+                    width={200}
+                    alt="testing"
+                    src="/srinivasan.jpg"
+                  />
+                </div>
+              </div> */}
+            </div>
           </AccordionDetails>
         </Accordion>
         <Accordion
-          expanded={expanded === 'panel2'}
-          onChange={handleChange('panel2')}
+          expanded={expanded === "panel2"}
+          onChange={handleChange("panel2")}
         >
           <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
-            <Typography>Collapsible Group Item #2</Typography>
+            <Typography
+              sx={{
+                color: "white",
+                fontSize: { xs: "1rem", sm: "1.35rem" },
+              }}
+            >
+              Technological Innovations - The driving factors and Impact
+            </Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
+            <div>
+              <div className="flex flex-col sm:flex-row  mx-auto justify-center items-center gap-5 md:gap-10 lg:gap-[3.5rem]">
+                <div className="flex flex-col-reverse justify-center items-center gap-3">
+                  <p className="text-white uppercase text-center text-[1rem]">
+                    Rajesh Shenoy
+                  </p>
+                  <Image
+                    height={200}
+                    width={200}
+                    alt="testing"
+                    src="/rajesh.jpg"
+                  />
+                </div>
+                <div className="flex flex-col-reverse justify-center items-center gap-3">
+                  <p className="text-white uppercase text-center text-[1rem]">
+                    Saumy Rajan Pradhan 
+                  </p>
+                  <Image
+                    height={200}
+                    width={200}
+                    alt="testing"
+                    src="/Saumy.jpg"
+                  />
+                </div>
+                {/* <div className="flex flex-col-reverse justify-center items-center gap-3">
+                  <p className="text-white uppercase text-center text-[1rem]">
+                    N Srinivasan
+                  </p>
+                  <Image
+                    height={200}
+                    width={200}
+                    alt="testing"
+                    src="/srinivasan.jpg"
+                  />
+                </div> */}
+              </div>
+              {/* <div className="flex flex-col sm:flex-row mx-auto justify-center items-center gap-5 md:gap-10 lg:gap-[3.5rem mt-10">
+                <div className="flex flex-col-reverse justify-center items-center gap-3">
+                  <p className="text-white uppercase text-center text-[1rem]">
+                    N Srinivasan
+                  </p>
+                  <Image
+                    height={200}
+                    width={200}
+                    alt="testing"
+                    src="/srinivasan.jpg"
+                  />
+                </div>
+                <div className="flex flex-col-reverse justify-center items-center gap-3">
+                  <p className="text-white uppercase text-center text-[1rem]">
+                    N Srinivasan
+                  </p>
+                  <Image
+                    height={200}
+                    width={200}
+                    alt="testing"
+                    src="/srinivasan.jpg"
+                  />
+                </div>
+              </div> */}
+            </div>
           </AccordionDetails>
         </Accordion>
         <Accordion
-          expanded={expanded === 'panel3'}
-          onChange={handleChange('panel3')}
+          expanded={expanded === "panel3"}
+          onChange={handleChange("panel3")}
         >
           <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
-            <Typography>Collapsible Group Item #3</Typography>
+            <Typography
+              sx={{
+                color: "white",
+                fontSize: { xs: "1rem", sm: "1.35rem" },
+              }}
+            >
+              The Employer-Employee Paradox: Concurrency of Layoffs and Hiring
+            </Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
+            <div>
+              <div className="flex flex-col sm:flex-row  mx-auto justify-center items-center gap-5 md:gap-10 lg:gap-[3.5rem]">
+                <div className="flex flex-col-reverse justify-center items-center gap-3">
+                  <p className="text-white uppercase text-center text-[1rem]">
+                    Yugandhar Penubolu
+                  </p>
+                  <Image
+                    height={200}
+                    width={200}
+                    alt="testing"
+                    src="/Yugandhar.jpg"
+                  />
+                </div>
+                {/* <div className="flex flex-col-reverse justify-center items-center gap-3">
+                  <p className="text-white uppercase text-center text-[1rem]">
+                    N Srinivasan
+                  </p>
+                  <Image
+                    height={200}
+                    width={200}
+                    alt="testing"
+                    src="/srinivasan.jpg"
+                  />
+                </div> */}
+                {/* <div className="flex flex-col-reverse justify-center items-center gap-3">
+                  <p className="text-white uppercase text-center text-[1rem]">
+                    N Srinivasan
+                  </p>
+                  <Image
+                    height={200}
+                    width={200}
+                    alt="testing"
+                    src="/srinivasan.jpg"
+                  />
+                </div> */}
+              </div>
+              {/* <div className="flex flex-col sm:flex-row mx-auto justify-center items-center gap-5 md:gap-10 lg:gap-[3.5rem mt-10">
+                <div className="flex flex-col-reverse justify-center items-center gap-3">
+                  <p className="text-white uppercase text-center text-[1rem]">
+                    N Srinivasan
+                  </p>
+                  <Image
+                    height={200}
+                    width={200}
+                    alt="testing"
+                    src="/srinivasan.jpg"
+                  />
+                </div>
+                <div className="flex flex-col-reverse justify-center items-center gap-3">
+                  <p className="text-white uppercase text-center text-[1rem]">
+                    N Srinivasan
+                  </p>
+                  <Image
+                    height={200}
+                    width={200}
+                    alt="testing"
+                    src="/srinivasan.jpg"
+                  />
+                </div>
+              </div> */}
+            </div>
           </AccordionDetails>
         </Accordion>
       </div>
--- pages/esummit.js
+++ pages/esummit.js
@@ -13,7 +13,7 @@ export default function Home() {
       {/* <Keynote /> */}
 
       <KeySpeakers />
-      {/* <Panel /> */}
+      <Panel />
       <Workshops />
       <Events />
       {/* <Speakers /> */}
