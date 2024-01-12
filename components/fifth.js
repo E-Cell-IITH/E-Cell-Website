a14ca70f0ff5b07595fc7f18dbd547c964030dc1
Maharshi <cs21btech11027@iith.ac.in>
2024-01-12 22:13:09 +0530

    number removed

--- components/fifth.js
+++ components/fifth.js
@@ -187,10 +187,6 @@ export default function Fifth({esummit = false}) {
               </div>
               <div className={style.list1} id={style.list3}>
                 (+91)90900 72712
-                <br />
-
-                (+91)8979471587
-                <br/>
                 (+91)72359 15379
 
               </div>
