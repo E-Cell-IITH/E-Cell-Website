430d21ef4a4160fc27659652b251610abc26e093
Bhaskar <bhaskarmandal369@gmail.com>
2024-07-22 02:13:48 +0530

    feat: freshers guid map done for mobile

--- components/guideCard.jsx
+++ components/guideCard.jsx
@@ -1,14 +1,38 @@
-import { Box } from "@mui/material";
-import React from "react";
+import React from 'react';
+import { Card, CardMedia, Typography, Box } from '@mui/material';
 
-function GuideCard() {
+const GuideCard = ({ title, description, image, latitude, longitude, width }) => {
+    const openGoogleMap = () => {
+        const url = `https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}`;
+        window.open(url, '_blank');
+    }
+
     return (
-        <Box style={{
-            background: "red"
-        }} height={210} width={280}  borderRadius={28} variant="outlined">
-            
-        </Box>
-    )
-}
+        <Card sx={{
+            zIndex: 1000,
+            border: '#5B5A5A solid 1px',
+            display: 'flex', alignItems: 'center', height: 64, width: width ? width : 190, borderRadius: '7px', overflow: 'hidden', backgroundColor: '#3A3C43', color: '#fff'
+        }}>
+            <CardMedia
+                component="img"
+                sx={{ width: 54, height: 54, ml: "6px" }}
+                image={image}
+                alt="Food Court"
+            />
+            <Box sx={{ ml: "6px", display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
+                <Typography variant="h5" component="div" sx={{ fontFamily: 'Montserrat', fontSize: "11px", fontWeight: 'bold' }}>
+                    {title}
+                </Typography>
+                <Typography variant="subtitle1" sx={{ fontFamily: 'Montserrat', fontSize: "7px", fontWeight: '400' }}>
+                    {description}
+                </Typography>
+
+                <button onClick={openGoogleMap} class="bg-[#45474D] text-white font-montserrat button-googleMap font-normal py-0.5 px-4 rounded outline-none focus:outline-none hover:bg-[#56585D] transition-colors">
+                    Show in Google Maps
+                </button>
+            </Box>
+        </Card>
+    );
+};
 
 export default GuideCard;
--- pages/test1.js
+++ pages/test1.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import GuideCard from '../components/guideCard';
 
 const SvgPage = () => (
     <>
         <div className='min-h-screen flex flex-col items-center'>
-            <img src='/page.svg' className='full-width-svg' alt="Page SVG" />
+            {/* <img src='/page.svg' className='full-width-svg' alt="Page SVG" /> */}
+            <GuideCard title="Food Court"
+                description="Domino's, Roll Zone and Barrista Cafe."
+                image="/guide/foodCourt.png" />
         </div>
     </>
 );
