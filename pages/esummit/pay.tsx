070b90991814db40c68620ccbab5a7699adbf8a2
SRodriguezIITH <sonitnitin@gmail.com>
2025-01-22 01:51:27 +0530

    feat:Payment Portal Frontend and integration

---
+++ pages/esummit/pay.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import { Button } from '@mui/material';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+import PaymentPortal from './ecpay.jsx'; // Import your PaymentPortal component
+
+import { useMediaQuery } from "@mui/material";
+
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & { children: React.ReactElement<any, any> },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function PayDialogSlide({ open, onClose, title, price, logoLink, width }) {
+  return (
+    <Dialog
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={onClose}
+      aria-describedby="alert-dialog-slide-description"
+      fullWidth // Makes the dialog take the full width defined by `maxWidth`
+      maxWidth="md" // Set the desired maxWidth ('xs', 'sm', 'md', 'lg', 'xl')
+      sx={{
+        '& .MuiDialog-paper': {
+          width: {width}, // Custom width
+          maxWidth: 'none', // Disable Material-UI's default max-width constraint
+          overflowY: 'hidden', // Disable Material-UI's default overflow-x: hidden
+        },
+      }}
+    >
+      
+      <DialogContent>
+        <PaymentPortal title={title} price={price} logoLink={logoLink} />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Cancel Transaction</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
